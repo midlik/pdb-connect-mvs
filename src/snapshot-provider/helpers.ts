8db3bb099685752f02466a601e99fc1c2100ee6f
Adam Midlik <midlik@gmail.com>
2025-02-06 13:14:06 +0000

    Reorg files

---
+++ src/snapshot-provider/helpers.ts
@@ -0,0 +1,151 @@
+import * as Builder from 'molstar/lib/extensions/mvs/tree/mvs/mvs-builder';
+import { IonNames } from 'molstar/lib/mol-model/structure/model/types/ions';
+import { LipidNames } from 'molstar/lib/mol-model/structure/model/types/lipids';
+import { SaccharideNames } from 'molstar/lib/mol-model/structure/model/types/saccharides';
+import { ElementSymbolColors } from 'molstar/lib/mol-theme/color/element-symbol';
+import { Color } from 'molstar/lib/mol-util/color';
+import { cycleIterator, ENTITY_COLORS, LIGAND_COLORS } from './colors';
+import { EntityRecord, ModifiedResidueRecord } from './data-provider';
+
+
+
+export type StandardComponentType = 'polymer' | 'branched' | 'branchedLinkage' | 'ligand' | 'ion' | 'nonstandard' | 'water';
+export type LigEnvComponentType = 'ligand' | 'environment' | 'wideEnvironment' | 'linkage';
+export type StandardVisualType = 'polymerCartoon' | 'branchedCarbohydrate' | 'branchedSticks' | 'branchedLinkageSticks' | 'ligandSticks' | 'ionSticks' | 'nonstandardSticks' | 'waterSticks';
+export type LigEnvVisualType = 'ligandSticks' | 'environmentSticks' | 'linkageSticks' | 'wideEnvironmentCartoon';
+
+
+
+export function decideEntityType(entityInfo: EntityRecord): StandardComponentType {
+    if (entityInfo.type === 'water') {
+        return 'water';
+    }
+    if (entityInfo.type === 'bound') {
+        if (entityInfo.compIds.length === 1 && SaccharideNames.has(entityInfo.compIds[0])) {
+            // TODO should we treat lipids in a special way? src/mol-model/structure/model/types/lipids.ts
+            return 'branched';
+        } else if (entityInfo.compIds.length === 1 && IonNames.has(entityInfo.compIds[0])) {
+            return 'ion';
+        } else {
+            return 'ligand';
+        }
+    }
+    if (entityInfo.type === 'carbohydrate polymer') { // TODO check what values `type` can have
+        return 'branched';
+    }
+    // TODO all types
+    return 'polymer';
+}
+
+export interface StandardComponentsOptions {
+    modifiedResidues: ModifiedResidueRecord[],
+}
+
+export const StardardComponents: { [type in StandardComponentType]?: (struct: Builder.Structure, options: StandardComponentsOptions) => Builder.Component } = {
+    polymer(structure: Builder.Structure) {
+        return structure.component({ selector: 'polymer' });
+    },
+    branched(structure: Builder.Structure) {
+        return structure.component({ selector: 'branched' });
+    },
+    branchedLinkage: undefined, // TODO select sugar linkage somehow if we want it (const sugarLinkageSticks = await this.nodes.branchedLinkage?.makeBallsAndSticks(options, ['branchedLinkageSticks']);)
+    ligand(structure: Builder.Structure) {
+        return structure.component({ selector: 'ligand' });
+    },
+    ion(structure: Builder.Structure) {
+        return structure.component({ selector: 'ion' });
+    },
+    nonstandard(structure: Builder.Structure, options: StandardComponentsOptions) {
+        return structure.component({ selector: options.modifiedResidues.map(r => ({ label_asym_id: r.labelAsymId, label_seq_id: r.labelSeqId })) });
+    },
+};
+
+export interface StandardRepresentationsOptions {
+    opacityFactor?: number,
+}
+
+export const StandardRepresentations: { [type in StandardComponentType]?: (comp: Builder.Component, options: StandardRepresentationsOptions) => { [repr in StandardVisualType]?: Builder.Representation } } = {
+    polymer(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            polymerCartoon: applyOpacity(component.representation({ type: 'cartoon' }), options.opacityFactor),
+        };
+    },
+    branched(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            branchedCarbohydrate: applyOpacity(component.representation({ type: 'surface' }), 0.5 * (options.opacityFactor ?? 1)), // TODO add SNFG to MVS
+            branchedSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), 0.8 * (options.opacityFactor ?? 1)),
+        };
+    },
+    branchedLinkage(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            branchedLinkageSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
+        };
+    },
+    ligand(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            ligandSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
+        };
+    },
+    ion(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            ionSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
+        };
+    },
+    nonstandard(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            nonstandardSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
+        };
+    },
+    water(component: Builder.Component, options: StandardRepresentationsOptions) {
+        return {
+            waterSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
+        };
+    },
+};
+
+
+export function applyStandardReprs(struct: Builder.Structure, options: { modifiedResidues: ModifiedResidueRecord[], opacityFactor?: number }) {
+    const reprs: { [repr in StandardVisualType]?: Builder.Representation } = {};
+    let comp: StandardComponentType;
+    for (comp in StardardComponents) {
+        const component = StardardComponents[comp]?.(struct, options);
+        if (!component) continue;
+        const representations = StandardRepresentations[comp]?.(component, options);
+        for (const rep in representations) {
+            reprs[rep as StandardVisualType] = representations[rep as keyof typeof representations];
+        }
+    }
+    return reprs;
+}
+
+export function applyEntityColors(repr: Builder.Representation, colors: { [entityId: string]: Color }) {
+    for (const entityId in colors) {
+        repr.color({
+            selector: { label_entity_id: entityId },
+            color: Color.toHexStyle(colors[entityId]) as any,
+        });
+    }
+}
+
+export function applyOpacity(repr: Builder.Representation, opacity: number | undefined) {
+    if (opacity !== undefined && opacity !== 1) return repr.opacity({ opacity });
+    else return repr;
+}
+
+export function getEntityColors(entities: { [entityId: string]: EntityRecord }): { [entityId: string]: Color } {
+    const polymerColorIterator = cycleIterator(ENTITY_COLORS);
+    const ligandColorIterator = cycleIterator(LIGAND_COLORS);
+    const waterColor = ElementSymbolColors.O;
+
+    const out: { [entityId: string]: Color } = {};
+
+    for (const entityId of Object.keys(entities)) {
+        const entity = entities[entityId];
+        out[entityId] = entity.type === 'water' ? waterColor : entity.type === 'bound' ? ligandColorIterator.next().value! : polymerColorIterator.next().value!;
+        // TODO assign fixed colors to single-element ligands? (like in PDBImages)
+    }
+    return out;
+}
+
+
+
--- src/snapshot-provider/mvs-snapshot-provider.ts
+++ src/snapshot-provider/mvs-snapshot-provider.ts
@@ -1,10 +1,8 @@
 import { MVSData, MVSData_State } from 'molstar/lib/extensions/mvs/mvs-data';
 import * as Builder from 'molstar/lib/extensions/mvs/tree/mvs/mvs-builder';
-import { ElementSymbolColors } from 'molstar/lib/mol-theme/color/element-symbol';
 import { Color } from 'molstar/lib/mol-util/color';
-import { cycleIterator, ENTITY_COLORS, LIGAND_COLORS } from './colors';
-import { SaccharideNames } from 'molstar/lib/mol-model/structure/model/types/saccharides';
-import { ApiDataProvider, EntityRecord, IDataProvider, ModifiedResidueRecord } from './data-provider';
+import { ApiDataProvider, IDataProvider } from './data-provider';
+import { applyEntityColors, applyStandardReprs, decideEntityType, getEntityColors, StandardRepresentations } from './helpers';
 
 
 type SnapshotSpecParams = {
@@ -81,8 +79,8 @@ export class MVSSnapshotProvider {
                 break;
         }
         description.push('---');
-        description.push(`**View kind:** ${spec.kind}`);
-        description.push(`**View params:** ${JSON.stringify(spec.params)}`);
+        description.push(`- **View kind:** ${spec.kind}`);
+        description.push(`- **View params:** ${JSON.stringify(spec.params)}`);
         return builder.getState({ title: spec.name, description: description.join('\n\n') });
     }
 
@@ -148,13 +146,13 @@ export class MVSSnapshotProvider {
         const entityType = decideEntityType(entities[params.entityId]);
         const entityComponent = struct.component({ selector: { label_entity_id: params.entityId } });
         const entityReprs = StandardRepresentations[entityType]?.(entityComponent, {}) ?? {};
-        // .representation({ type: entityType === 'polymer' ? 'cartoon' : 'ball_and_stick' }) // TODO treat each entity type appropriately
         for (const repr of Object.values(entityReprs)) {
             repr.color({ color: Color.toHexStyle(entityColors[params.entityId]) as any });
         }
-        // TODO decide repr type based on entity type
 
         outDescription.push(`## Entity ${params.entityId}`);
+        const entityName = entities[params.entityId].names[0];
+        outDescription.push((entityName ? `__${entityName}__` : '*Entity name not available*') + ` (${entityType})`);
         const src = theAssembly === preferredAssembly
             ? `assembly ${theAssembly} (preferred)`
             : theAssembly !== undefined
@@ -165,148 +163,6 @@ export class MVSSnapshotProvider {
 }
 
 
-type StandardComponentType = 'polymer' | 'branched' | 'branchedLinkage' | 'ligand' | 'ion' | 'nonstandard' | 'water';
-type LigEnvComponentType = 'ligand' | 'environment' | 'wideEnvironment' | 'linkage';
-type StandardVisualType = 'polymerCartoon' | 'branchedCarbohydrate' | 'branchedSticks' | 'branchedLinkageSticks' | 'ligandSticks' | 'ionSticks' | 'nonstandardSticks' | 'waterSticks';
-type LigEnvVisualType = 'ligandSticks' | 'environmentSticks' | 'linkageSticks' | 'wideEnvironmentCartoon';
-
-
-
-function decideEntityType(entityInfo: EntityRecord): StandardComponentType {
-    if (entityInfo.type === 'water') {
-        return 'water';
-    }
-    if (entityInfo.type === 'bound') {
-        if (entityInfo.compIds.length === 1 && SaccharideNames.has(entityInfo.compIds[0])) {
-            // TODO should we treat lipids in a special way? src/mol-model/structure/model/types/lipids.ts
-            return 'branched';
-        } else {
-            return 'ligand';
-        }
-    }
-    if (entityInfo.type === 'carbohydrate polymer') { // TODO check what values `type` can have
-        return 'branched';
-    }
-    // TODO all types
-    return 'polymer';
-}
-
-interface StandardComponentsOptions {
-    modifiedResidues: ModifiedResidueRecord[],
-}
-
-const StardardComponents: { [type in StandardComponentType]?: (struct: Builder.Structure, options: StandardComponentsOptions) => Builder.Component } = {
-    polymer(structure: Builder.Structure) {
-        return structure.component({ selector: 'polymer' });
-    },
-    branched(structure: Builder.Structure) {
-        return structure.component({ selector: 'branched' });
-    },
-    branchedLinkage: undefined, // TODO select sugar linkage somehow if we want it (const sugarLinkageSticks = await this.nodes.branchedLinkage?.makeBallsAndSticks(options, ['branchedLinkageSticks']);)
-    ligand(structure: Builder.Structure) {
-        return structure.component({ selector: 'ligand' });
-    },
-    ion(structure: Builder.Structure) {
-        return structure.component({ selector: 'ion' });
-    },
-    nonstandard(structure: Builder.Structure, options: StandardComponentsOptions) {
-        return structure.component({ selector: options.modifiedResidues.map(r => ({ label_asym_id: r.labelAsymId, label_seq_id: r.labelSeqId })) });
-    },
-};
-
-interface StandardRepresentationsOptions {
-    opacityFactor?: number,
-}
-
-const StandardRepresentations: { [type in StandardComponentType]?: (comp: Builder.Component, options: StandardRepresentationsOptions) => { [repr in StandardVisualType]?: Builder.Representation } } = {
-    polymer(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            polymerCartoon: applyOpacity(component.representation({ type: 'cartoon' }), options.opacityFactor),
-        };
-    },
-    branched(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            branchedCarbohydrate: applyOpacity(component.representation({ type: 'surface' }), 0.5 * (options.opacityFactor ?? 1)), // TODO add SNFG to MVS
-            branchedSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), 0.8 * (options.opacityFactor ?? 1)),
-        };
-    },
-    branchedLinkage(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            branchedLinkageSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
-        };
-    },
-    ligand(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            ligandSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
-        };
-    },
-    ion(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            ionSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
-        };
-    },
-    nonstandard(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            nonstandardSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
-        };
-    },
-    water(component: Builder.Component, options: StandardRepresentationsOptions) {
-        return {
-            waterSticks: applyOpacity(component.representation({ type: 'ball_and_stick' }), options.opacityFactor),
-        };
-    },
-};
-
-
-function applyStandardReprs(struct: Builder.Structure, options: { modifiedResidues: ModifiedResidueRecord[], opacityFactor?: number }) {
-    const reprs: { [repr in StandardVisualType]?: Builder.Representation } = {};
-    let comp: StandardComponentType;
-    for (comp in StardardComponents) {
-        const component = StardardComponents[comp]?.(struct, options);
-        if (!component) continue;
-        const representations = StandardRepresentations[comp]?.(component, options);
-        for (const rep in representations) {
-            reprs[rep as StandardVisualType] = representations[rep as keyof typeof representations];
-        }
-    }
-    return reprs;
-}
-
-function applyEntityColors(repr: Builder.Representation, colors: { [entityId: string]: Color }) {
-    for (const entityId in colors) {
-        repr.color({
-            selector: { label_entity_id: entityId },
-            color: Color.toHexStyle(colors[entityId]) as any,
-        });
-    }
-}
-
-function applyOpacity(repr: Builder.Representation, opacity: number | undefined) {
-    if (opacity !== undefined && opacity !== 1) return repr.opacity({ opacity });
-    else return repr;
-}
-
-function getEntityColors(entities: { [entityId: string]: EntityRecord }): { [entityId: string]: Color } {
-    const polymerColorIterator = cycleIterator(ENTITY_COLORS);
-    const ligandColorIterator = cycleIterator(LIGAND_COLORS);
-    const waterColor = ElementSymbolColors.O;
-
-    const out: { [entityId: string]: Color } = {};
-
-    for (const entityId of Object.keys(entities)) {
-        const entity = entities[entityId];
-        out[entityId] = entity.type === 'water' ? waterColor : entity.type === 'bound' ? ligandColorIterator.next().value! : polymerColorIterator.next().value!;
-        // TODO assign fixed colors to single-element ligands? (like in PDBImages)
-    }
-    return out;
-}
-
-
-
-
-
-
-
 /** Return a new MVSSnapshotProvider taking data from PDBe API (https://www.ebi.ac.uk/pdbe/api) */
 export function getDefaultMVSSnapshotProvider(): MVSSnapshotProvider {
     console.log('getDefaultMVSSnapshotProvider')
@@ -314,7 +170,10 @@ export function getDefaultMVSSnapshotProvider(): MVSSnapshotProvider {
     return new MVSSnapshotProvider(dataProvider);
 }
 
+
 /*
+NOTES:
+
 All existing PDBImages states:
 - Entry
 - Assembly
@@ -334,4 +193,5 @@ New states:
 
 More ideas:
 - Custom highlight granularity in Molstar (to pick e.g. domains)
+
 */
